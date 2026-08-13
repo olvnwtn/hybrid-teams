72458eb7bed1b33c26c41b38a4a0d5c2fbdc009d
Olivia B. Newton <olivia.newton@umt.edu>
2026-08-12 20:17:18 -0600

    Create 00_packages.R

---
+++ scripts/00_packages.R
@@ -0,0 +1,35 @@
+# Author: Olivia B. Newton
+# Purpose: Load packages for analysis pipeline. Versions are recorded by renv.
+
+# Packages ----------------------------------------------------------------
+
+
+library(tidyverse)
+
+library(corrplot)
+library(DescTools)
+library(distributions3)
+library(fastDummies)
+library(flexplot)
+library(gganimate)
+library(ggdist)
+library(ggpubr)
+library(ggsci)
+library(hrbrthemes)
+library(Hmisc)
+library(likert)
+library(lsr)
+library(mediation)
+library(multilevel)
+library(openxlsx)
+library(pracma)
+library(psych)
+library(RColorBrewer)
+library(readxl)
+library(reshape)
+library(reshape2)
+library(rstatix)
+library(skimr)
+library(tidyquant)
+library(TOSTER)
+library(viridis)
