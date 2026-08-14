a845b61592aa62955c92f67216adf1b51ffb5985
Olivia B. Newton <olivia.newton@umt.edu>
2026-08-14 14:07:51 -0600

    Update 00_packages.R

--- scripts/00_packages.R
+++ scripts/00_packages.R
@@ -1,16 +1,14 @@
 # Author: Olivia B. Newton
-# Purpose: Load packages for analysis pipeline. Versions are recorded by renv.
+# Purpose: Load packages for analysis pipeline. 
+#.         Versions are recorded by renv.
 
 # Packages ----------------------------------------------------------------
-
-
-library(tidyverse)
 
 library(corrplot)
 library(DescTools)
 library(distributions3)
 library(fastDummies)
-library(flexplot)
+#library(flexplot)
 library(gganimate)
 library(ggdist)
 library(ggpubr)
@@ -21,7 +19,7 @@ library(hrbrthemes)
 library(likert)
 library(lsr)
 library(mediation)
-library(multilevel)
+#library(multilevel)
 library(openxlsx)
 library(pracma)
 library(psych)
@@ -31,6 +29,15 @@ library(reshape)
 library(reshape2)
 library(rstatix)
 library(skimr)
+library(tidyverse)
 library(tidyquant)
 library(TOSTER)
 library(viridis)
+
+library(conflicted)
+
+conflict_prefer("recode", "dplyr")
+conflict_prefer("rename", "dplyr")
+conflict_prefer("filter", "dplyr")
+conflict_prefer("count", "dplyr")
+conflict_prefer("select", "dplyr")
