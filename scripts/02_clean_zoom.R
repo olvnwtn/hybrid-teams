15131cddf276358ef3f7188511c7f962c2ce8c3f
Olivia B. Newton <olivia.newton@umt.edu>
2026-08-17 23:56:59 -0600

    Create 02_clean_zoom.R

---
+++ scripts/02_clean_zoom.R
@@ -0,0 +1,12 @@
+# Authors: Olivia B. Newton and Tyler R. Talbot
+# Purpose: Clean the Zoom expert participation data.
+
+# Clean Zoom data ---------------------------------------------------------
+
+zoom_clean <- zoom_raw |>
+  rename(team_id = session_id,
+         exp_part_in_zoom = participation_secs)
+
+# Save processed data -----------------------------------------------------
+
+write_csv(zoom_clean, here("data", "processed", "zoom.csv"))
